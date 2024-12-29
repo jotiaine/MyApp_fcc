7547bf846762a9e60c3aeb69fcb9aa69dda9a6ca
Joni Tiainen <jonitia91@gmail.com>
2024-12-29 11:58:47 +0200

    Delete method added.

--- MyApp_fcc/Controllers/ItemsController.cs
+++ MyApp_fcc/Controllers/ItemsController.cs
@@ -41,5 +41,35 @@ namespace MyApp_fcc.Controllers
             var item = await _context.Items.FirstOrDefaultAsync(x=>x.Id == id);
             return View(item);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            { 
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        { 
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if (item != null)
+            { 
+                _context.Items.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
